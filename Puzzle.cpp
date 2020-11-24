02a6538c5f78c177075b59743d5a24700b757ac0
ppinko <p.pinkowicz@gmail.com>
2020-11-24 16:34:24 +0100

    DEVELOP - Add class Puzzle
    Class to hold initial and current state of puzzle as well as a solution.

---
+++ Puzzle.cpp
@@ -0,0 +1,33 @@
+#include "Puzzle.h"
+
+Puzzle::Puzzle(std::vector<std::vector<int>> _puzzle):
+    puzzle {_puzzle}, initialPuzzle {_puzzle}, solution {_puzzle}
+{
+};
+
+void Puzzle::PrintState(std::vector<std::vector<int>> const &state){
+    auto const width = 9U;
+    for (size_t i = 0; i < width; ++i)
+    {
+        for (int j = 0; j < width; ++j)
+        {
+            if (state[i][j] == 0)
+                std::cout << "-" << " ";
+            else
+                std::cout << state[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
+};
+
+void Puzzle::PrintInitialPuzzle(){
+    PrintState(initialPuzzle);
+};
+
+void Puzzle::PrintCurrentState(){
+    PrintState(puzzle);
+};
+
+void Puzzle::PrintSolution(){
+    PrintState(solution);
+};
---
+++ Puzzle.h
@@ -0,0 +1,29 @@
+#ifndef PUZZLE_H
+#define PUZZLE_H
+
+#include <set>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <queue>
+
+class Puzzle
+{
+private:
+    const std::vector<std::vector<int>> initialPuzzle;
+    std::vector<std::vector<int>> puzzle;
+    std::vector<std::vector<int>> solution;
+
+    void PrintState(std::vector<std::vector<int>> const &state);
+
+public:
+	Puzzle(std::vector<std::vector<int>> _puzzle);
+    
+    void PrintInitialPuzzle();
+
+    void PrintCurrentState();
+
+    void PrintSolution();
+};
+
+#endif
